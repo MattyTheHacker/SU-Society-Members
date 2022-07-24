96bbac5f85098d55f151d8d154540ee9eeaae68c
MattyTheHacker <18513864+MattyTheHacker@users.noreply.github.com>
2022-07-24 23:44:08 +0100

    record cleanup

--- src/main/java/Member.java
+++ src/main/java/Member.java
@@ -2,16 +2,6 @@ import java.time.LocalDateTime;
 
 public record Member(String name, int id, LocalDateTime joinDate, LocalDateTime expireDate) {
 
-    @Override
-    public String toString() {
-        return "Member{" +
-                "name='" + name + '\'' +
-                ", id=" + id +
-                ", joinDate=" + joinDate +
-                ", expireDate=" + expireDate +
-                '}';
-    }
-
     public String getFirstName() {
         // name format: surname, first name
         String[] nameParts = name.split(",");
