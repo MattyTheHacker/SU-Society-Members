23829ae1890dcb9e9a6ab3913bad0c2bb0ca7b77
MattyTheHacker <18513864+MattyTheHacker@users.noreply.github.com>
2022-07-24 23:43:21 +0100

    convert member object to record

--- src/main/java/Member.java
+++ src/main/java/Member.java
@@ -1,17 +1,6 @@
 import java.time.LocalDateTime;
 
-public class Member {
-    private final String name;
-    private final int id;
-    private final LocalDateTime joinDate;
-    private final LocalDateTime expireDate;
-
-    public Member(String name, int id, LocalDateTime joinDate, LocalDateTime expireDate) {
-        this.name = name;
-        this.id = id;
-        this.joinDate = joinDate;
-        this.expireDate = expireDate;
-    }
+public record Member(String name, int id, LocalDateTime joinDate, LocalDateTime expireDate) {
 
     @Override
     public String toString() {
@@ -21,22 +10,6 @@ public class Member {
                 ", joinDate=" + joinDate +
                 ", expireDate=" + expireDate +
                 '}';
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public LocalDateTime getJoinDate() {
-        return joinDate;
-    }
-
-    public LocalDateTime getExpireDate() {
-        return expireDate;
     }
 
     public String getFirstName() {
