bc612906712f4eace3059edd4e17236fb06daf46
MattyTheHacker <matty258987@gmail.com>
2022-07-24 21:55:28 +0100

    fix java

--- src/main/java/GetSocMembers.java
+++ src/main/java/GetSocMembers.java
@@ -10,17 +10,19 @@ import java.net.http.HttpResponse;
 public class GetSocMembers {
     private static final Dotenv dotenv = Dotenv.load();
     private static final String SOC_MEMBERS_URL = dotenv.get("UNION_URL");
-    private static final String SOC_COMMITTEE_COOKIE = dotenv.get("UNION_COOKIE");
+    private static final String SOC_COMMITTEE_COOKIE = ".ASPXAUTH=" + dotenv.get("UNION_COOKIE");
+
 
     public static void main(String[] args) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(SOC_MEMBERS_URL))
                 .header("Cookie", SOC_COMMITTEE_COOKIE)
-                .GET().
-                build();
+                .GET()
+                .build();
+
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.toString());
+        System.out.println(response.body());
     }
 }
 
